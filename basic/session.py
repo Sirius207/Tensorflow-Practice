35eea895fbd86f5a16a2ef8c27a7eb9dadb1dd24
Po Chun, Lu <oceanus11034@gmail.com>
2017-09-15 12:56:42 +0800

    [Add] basic session practice

---
+++ basic/session.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+# create two matrix
+
+matrix1 = tf.constant([[3, 3]])
+matrix2 = tf.constant([[2], [2]])
+
+# [3, 3] * [2; 2] = [12] 
+product = tf.matmul(matrix1, matrix2)
+
+
+# methid 1
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+sess.close()
+# [[12]]
+
+
+# method 2
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result)
+
+# [[12]]
